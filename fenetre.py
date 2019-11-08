80012ef97c9eddc18e64451224e4b293e4cd3457
anat <49984698+anat3000@users.noreply.github.com>
2019-11-08 13:59:17 +0100

    Update fenetre.py

--- fenetre.py
+++ fenetre.py
@@ -1,6 +1,7 @@
+import webbrowser
+
 from tkinter import *
 from tkinter.messagebox import *
-root = Tk()
 
 def nondisp():
     showerror("En cours de développement...", "Nous y travaillons...")
@@ -8,8 +9,18 @@ def nondisp():
 def apropos():
     showinfo("A propos - Version", "Version : 0.0.1\nCe n'est que le commencement...")
 
-##barre de menu
-    
+def askquit():
+    if askyesno("Do you want to quit ?", "Do you want to quit ?"):
+        root.quit()
+
+def ch():
+    menu3.add_command(label="A propos", command=apropos)
+
+root = Tk()
+root.iconbitmap("xlogo64.xbm")
+
+#barre de menu
+
 menubar = Menu(root)
 
 #fichier
@@ -17,16 +28,18 @@ menu1 = Menu(menubar, tearoff=0)
 menu1.add_command(label="Créer", command=nondisp)
 menu1.add_command(label="Editer", command=nondisp)
 menu1.add_separator()
-menu1.add_command(label="Quitter", command=root.quit)
+menu1.add_command(label="Quitter", command=askquit)
 
 menubar.add_cascade(label="Fichier", menu=menu1)
 
 #aide
 menu3 = Menu(menubar, tearoff=0)
-menu3.add_command(label="A propos", command=apropos)
+menu3.add_command(label="Changelog", command=apropos)
+menu3.add_command(label="A propos", command=ch)
 
 menubar.add_cascade(label="Aide", menu=menu3)
 
 root.config(menu=menubar)
 
 root.mainloop()
+exit(0)
